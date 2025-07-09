c0ac61604cc214b7d2e7631245a7a14c6527c8e5
LEO STRIETZEL <leostrietzel@gmail.com>
2025-07-09 13:07:32 +0200

    update custom prompt

--- custom_prompt.py
+++ custom_prompt.py
@@ -7,7 +7,7 @@ You are an expert analyst for ESG reports.
 
 Use the context below to answer the question. 
 Always cite the page number(s) in parentheses after each fact, e.g., "(Page 3)".
-If you don't know, say "I don't know".
+If you don't know, say "I don't know" and provide the most likely explaination.
 
 Context:
 {context}
